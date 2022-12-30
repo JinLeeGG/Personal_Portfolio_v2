abeb5f4f89c7443e954a61cda7152cdd22798747
GyuJin Lee <gjlee99211@gmail.com>
2022-12-30 18:56:14 -0500

    Typewriting effect added

--- Client/src/PortfolioContainer/Home/Profile.js
+++ Client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Typical from 'react-typical' 
 
 export default function Profile() {
   return (
@@ -23,8 +24,33 @@ export default function Profile() {
                     </a>
                 </div>
                 <div className='profile-details-name'>
+                    <span className='primary-text'>
                     {" "}
-                    Hello, I'M <span className='highlighted-text'>John</span> 
+                    Hello, I'M <span className='highlighted-text'>John</span>
+                    </span>
+                </div>
+                <div className='profile-details-role'>
+                    <span className='primary-text'>
+                        {" "}
+                        <h1>
+                            {" "}
+                            <Typical
+                            loop = {Infinity}
+                            steps = {[
+                                "Ethusiastic Dev ",
+                                1000,
+                                "Full Stack Developer",
+                                1000,
+                                "MERN Stack Dev",
+                                1000,
+                                "Cross Platform v",
+                                1000,
+                                "Mad Coder",
+                                1000,
+                            ]}
+                            />
+                        </h1>
+                    </span>
                 </div>
             </div>
         </div>

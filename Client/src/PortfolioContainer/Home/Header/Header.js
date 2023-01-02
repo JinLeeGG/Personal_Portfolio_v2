8382594d10b7165020a6864dd8b2e885ac83bcab
GyuJin Lee <gjlee99211@gmail.com>
2023-01-02 14:40:14 -0500

    Resume part

--- Client/src/PortfolioContainer/Home/Header/Header.js
+++ Client/src/PortfolioContainer/Home/Header/Header.js
@@ -35,10 +35,8 @@ export default function Header() {
 
   const getHeaderOptionsClasses = (index) => {
     let classes = "header-option ";
-    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator ";
-
-    if (selectedScreen === index) classes += "selected-header-option ";
-
+    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator";
+    if (selectedScreen === index) classes += "selected-header-option";
     return classes;
   };
 

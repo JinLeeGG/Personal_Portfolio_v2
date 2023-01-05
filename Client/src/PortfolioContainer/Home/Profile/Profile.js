d8b154ceabdd5542384d5e84ee78b625cf663a3f
GyuJin Lee <gjlee99211@gmail.com>
2023-01-04 22:57:19 -0500

    profile part edited

--- Client/src/PortfolioContainer/Home/Profile/Profile.js
+++ Client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -38,22 +38,26 @@ export default function Profile() {
                 <Typical
                   loop={Infinity}
                   steps={[
-                    "Ethusiastic Dev ✨",
-                    1000,
-                    "Full Stack Developer 📚",
-                    1000,
-                    "MERN Stack Dev 🔥",
-                    1000,
-                    "Cross Platform Dev ⭐",
-                    1000,
-                    "Mad Coder 💢",
-                    1000,
+                    "Front-end Developer 🖥️",
+                    1500,
+                    "Back-end Developer 🖱️",
+                    1500,
+                    "Software Engineer 🧑🏻‍💻",
+                    1500,
+                    "Cross Platform Dev 🖧",
+                    1500,
+                    "Passionate Coder💡",
+                    1500,
                   ]}
                 />
               </h1>
               <span className="profile-role-tagline">
-                Knack of building applications with front and back end
-                operations.
+                As a software engineer, I have a strong foundation in computer
+                science concepts and programming languages.
+                <br /> I am skilled in analyzing user and system requirements to
+                determine the appropriate design and development <br />
+                approach, and have experience designing, implementing, and
+                testing software components.
               </span>
             </span>
           </div>

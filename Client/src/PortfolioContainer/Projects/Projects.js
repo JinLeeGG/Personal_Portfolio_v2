f37ee05c0990de623fce2379c201ab5d69f289be
GyuJin Lee <gjlee99211@gmail.com>
2023-01-07 19:49:38 -0500

    project part debug

--- Client/src/PortfolioContainer/Projects/Projects.js
+++ Client/src/PortfolioContainer/Projects/Projects.js
@@ -7,38 +7,36 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
-import IMG1 from "../../assets/Project/code-1076536_1920.jpg";
-import IMG2 from "../../assets/Project/coding-924920_1920.jpg";
+import IMG1 from "../../assets/Project/portfolioV1_demo.jpg";
+import IMG2 from "../../assets/Project/portfolioV2_demo.jpg";
 import IMG3 from "../../assets/Project/laptop-2620118_1920.jpg";
 
 const projectsDetails = [
   {
-    title: "Personal Portfolio Website",
-    duration: { fromDate: "2022", toDate: "2023" },
-    link: "https://github.com/JinLeeGG/Personal_Portfolio-v1",
+    title: "Personal Portfolio Website v.1",
+    duration: "December 2022",
+    gitHubLink: "https://github.com/JinLeeGG/Personal_Portfolio-v1",
+    demoLink: "https://gyujinlee.netlify.app/",
     img: IMG1,
     description:
-      "A Personal Portfolio website to showcase all my details and projects at one place.",
-    subHeading: "Technologies Used: React JS, Bootsrap",
+      "A Personal Portfolio website to showcase all my details and projects at one place. (Technologies Used: React JS, Bootsrap)",
   },
   {
-    title: "Mobile E-shop ",
-    duration: { fromDate: "2020", toDate: "2021" },
-    link: "https://github.com/JinLeeGG/Personal_Portfolio_v2",
+    title: "Personal Portfolio Website v.2",
+    duration: "January 2023",
+    gitHubLink: "https://github.com/JinLeeGG/Personal_Portfolio_v2",
+    demoLink: "", //update it later
     img: IMG2,
     description:
-      "An ecommerce application designed to sell products online wth payment system integration",
-    subHeading:
-      "Technologies Used:  React Native, Mongo DB, Express Js, Node Js, Redux.",
+      "A Personal Portfolio website to showcase all my details and projects at one place. (Technologies Used: HTML, CSS, Java)",
   },
   {
-    title: "Ecommerce Website ",
-    duration: { fromDate: "2020", toDate: "2021" },
+    title: "Sample project",
+    duration: "January 2023",
+    gitHubLink: "", //update it later
+    demoLink: "", //update it later
     img: IMG3,
-    description:
-      "Online ecommerce website for showcasing and selling products onlne with payment system integration, both Paypal and Stripe",
-    subHeading:
-      "Technologies Used: Mongo DB, Epress Js, React Js, Node JS, Redux, Bootstrap.",
+    description: "Sample project description",
   },
 ];
 
@@ -58,17 +56,32 @@ export default function Projects(props) {
         onSlideChange={() => console.log("slide change")}
       >
         {projectsDetails.map(
-          ({ title, duration, description, subHeading, img, link }, index) => {
+          (
+            { title, duration, description, img, gitHubLink, demoLink },
+            index
+          ) => {
             return (
               <SwiperSlide key={index} className="projects">
                 <div className="project_img">
                   <img src={img} alt="" />
                 </div>
+                <div className="duration">{duration}</div>
                 <h5 className="title">{title}</h5>
                 <small className="description">{description}</small>
-                <a href={link}>
-                  <button className="btn highlighted-btn">Go to GitHub</button>
-                </a>
+                <div className="about-me-options">
+                  <a href={gitHubLink}>
+                    <button className="btn highlighted-btn1">
+                      Go to GitHub &nbsp;
+                      <i className="fa fa-github"></i>
+                    </button>
+                  </a>
+                  <a href={demoLink}>
+                    <button className="btn highlighted-btn2">
+                      See Demo &nbsp;
+                      <i class="fa fa-mouse-pointer" aria-hidden="true"></i>
+                    </button>
+                  </a>
+                </div>
               </SwiperSlide>
             );
           }
